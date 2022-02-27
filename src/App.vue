36ee08da4e291b72da06b8cdce47421a4f1762d7
Robby <riceslif@gmail.com>
2022-02-26 20:36:55 -0600

    Starting landing page

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,43 @@
 <template>
-  <main>
-    <button :id="mainh2" @click="changeId">What's 9+10</button>
-  </main>
+  <div class="container-fluid px-0" id="main">
+    <Navbar />
+    <div class="container-fluid" id="hero">
+      <div class="row">
+        <div class="col-md-4 ml-5">
+          <h1 class="title pt-3" style="font-size: 64px; padding-left:30px;">TeaDex</h1>
+          <h3 class="text pb-5" style="font-size: 18px; padding-left:40px;">Your first stop for all things tea.</h3>
+          <div class="py-5"/>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue'
+
 export default {
-  methods: {
-    changeId() {
-      this.id = blue;
-    }
+  name: 'App',
+  components: {
+    Navbar
   }
 }
-  
 </script>
 
 <style>
-.button #blue {
-  background-color: blue;
+html {
+   background-color: #FFE8D6; 
 }
-
-#mainh2 {
-  background-color: red;
+#hero {
+  background-color: #A5A58D;
 }
-
+#main {
+  background-color:#FFE8D6;
+}
+.title {
+  font-family: 'PT Sans', sans-serif;
+}
+.text {
+  font-family: 'PT Serif', serif;
+}
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+    <ul class="nav justify-content-center">
+        <li class="nav-item">
+            <a href="#" class="nav-link text">Home</a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link text">Quiz</a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link text">Index</a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link text">Info</a>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
+
+<style scoped>
+#navboy-container {
+    background-color: #FFE8D6 !important;
+}
+.nav-link {
+    color: #6B705C;
+    font-size: 18px;
+}
+a:hover {
+    color:#A5A58D !important;
+}
+a:visited {
+    color: #6B705C;
+}
+</style>
